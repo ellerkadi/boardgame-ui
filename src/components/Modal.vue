ee3c16159d1ecb4cd696571420e3f651b2ba2183
a1lep <aile.pent@gmail.com>
2024-12-03 14:38:37 +0200

    .

---
+++ src/components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <slot name="game name">
+      </slot>
+
+      <slot name="description">
+      </slot>
+
+      <slot name="game type">
+      </slot>
+
+      <slot name="location">
+      </slot>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
--- src/components/MyGames.vue
+++ src/components/MyGames.vue
@@ -1,4 +1,86 @@
 <template>
+
+  <h2>SEARCH</h2>
+
+  <div>
+    <h3>Search by Game</h3>
+    <input type="text" id="searchGameInput" placeholder="Enter game name"/>
+    <button @click="findGameByGamename">Search</button>
+
+    <thead v-if="findGameByGamenameResult.length > 0">
+    <tr>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Game Type</th>
+      <th>Location</th>
+      <th>Availability</th>
+    </tr>
+    </thead>
+    <tbody v-for="game in findGameByGamenameResult" :key="game.id">
+    <tr>
+      <td>{{ game.gamename }}</td>
+      <td>{{ game.description }}</td>
+      <td>{{ game.gametype }}</td>
+      <td>{{ game.location }}</td>
+      <td>{{ game.availability }}</td>
+    </tr>
+    </tbody>
+  </div>
+
+  <h3>Search by Availability</h3>
+  <select id="availabilitySelect" @change="findGameByAvailability">
+    <option value="">Select Availability</option>
+    <option value="true">Available</option>
+    <option value="false">Not Available</option>
+  </select>
+
+  <table>
+    <thead v-if="findGameByAvailabilityResult.length > 0">
+    <tr>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Game Type</th>
+      <th>Location</th>
+    </tr>
+    </thead>
+    <tbody v-for="game in findGameByAvailabilityResult" :key="game.id">
+    <tr>
+      <td>{{ game.gamename }}</td>
+      <td>{{ game.description }}</td>
+      <td>{{ game.gametype }}</td>
+      <td>{{ game.location }}</td>
+    </tr>
+    </tbody>
+  </table>
+
+  <h3>Search by Game Type</h3>
+  <select id="gametypeSelect" @change="findGameByGametype">
+    <option value="">Select Availability</option>
+    <option value="Games for children">Games for children</option>
+    <option value="Classic games">Classic games</option>
+    <option value="Family games">Family games</option>
+    <option value="Strategic games">Strategic games</option>
+  </select>
+
+  <table>
+    <thead>
+    <tr>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Location</th>
+      <th>Availability</th>
+    </tr>
+    </thead>
+    <tbody v-for="game in findGameByGametypeResult" :key="game.id">
+    <tr>
+      <td>{{ game.gamename }}</td>
+      <td>{{ game.description }}</td>
+      <td>{{ game.location }}</td>
+      <td>{{ game.availability }}</td>
+    </tr>
+    </tbody>
+  </table>
+
   <form @submit.prevent="addGame" class="d-flex align-items-center mb-4">
 
     <h2>Add new game</h2>
@@ -74,7 +156,59 @@
       <td>{{ item.location }}</td>
       <td>{{ item.availability }}</td>
       <td>
-        <button @click="deleteGameById(item.id)" class="btn btn-danger btn-sm action-button">Change</button>
+        <section class="modal hidden">
+          <div class="flex">
+            <img src="user.png" width="50px" height="50px" alt="user" />
+            <button class="btn-close">⨉</button>
+          </div>
+          <div>
+            <h3>Stay in touch</h3>
+            <p>
+              This is a dummy newsletter form so don't bother trying to test it. Not
+              that I expect you to, anyways. :)
+            </p>
+          </div>
+
+          <input type="email" id="email" placeholder="brendaneich@js.com" />
+          <button class="btn">Submit</button>
+        </section>
+
+        <div class="overlay hidden"></div>
+        <button class="btn btn-open">Change</button>
+
+     <!--   <div class = "modal fade"
+             id="updateGameModal" tabindex="-1" aria-labelledby="updateGameModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="updateGameModalLabel">Update Game</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <form @submit.prevent="submitUpdate">
+                  <div class="mb-3">
+                    <label for="gamename" class="form-label">Game Name</label>
+                    <input type="text" class="form-control" id="gamename" v-model="updatedGame.gamename">
+                  </div>
+                  <div class="mb-3">
+                    <label for="description" class="form-label">Description</label>
+                    <input type="text" class="form-control" id="description" v-model="updatedGame.description">
+                  </div>
+                  <div class="mb-3">
+                    <label for="gametype" class="form-label">Game Type</label>
+                    <input type="text" class="form-control" id="gametype" v-model="updatedGame.gametype">
+                  </div>
+                  <div class="mb-3">
+                    <label for="location" class="form-label">Location</label>
+                    <input type="text" class="form-control" id="location" v-model="updatedGame.location">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Save changes</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+-->
         <button @click="deleteGameById(item.id)" class="btn btn-danger btn-sm action-button">Remove</button>
       </td>
     </tr>
@@ -114,6 +248,7 @@
 
 <script>
 import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default {
   data: () => ({
@@ -121,13 +256,23 @@ export default {
     allGamesList: [],
     pendingGames: [],
     approvedGames: [],
+    findGameByGamenameResult: [],
+    findGameByAvailabilityResult: [],
+    findGameByGametypeResult: [],
     newGame: {
       gamename: "",
       description: "",
       location: "",
       gametype: "",
       availability: ""
-    }
+    },
+    updatedGame: {
+      gamename: "",
+      description: "",
+      location: "",
+      gametype: "",
+      availability: ""
+    },
   }),
 
   methods: {
@@ -147,6 +292,59 @@ export default {
       axios
           .get(`${this.api}/approvedGames`)
           .then((res) => (this.approvedGames = res.data))
+    },
+
+    fetchFindGameByGamename(gamename) {
+      if (!gamename) {
+        console.error("Game name is required to fetch data.");
+        return;
+      }
+      axios
+          .get(`${this.api}/findGameByGamename/${gamename}`)
+          .then((res) => {
+            this.findGameByGamenameResult = res.data;
+          });
+    },
+
+    findGameByGamename() {
+      const gamename = document.getElementById("searchGameInput").value.trim();
+      this.fetchFindGameByGamename(gamename);
+    },
+
+    fetchFindGameByAvailability(availability) {
+      axios
+          .get(`${this.api}/findGameByAvailability/${availability}`)
+          .then((res) => {
+            this.findGameByAvailabilityResult = res.data;
+            console.log("Games found:", res.data);
+          });
+    },
+
+    findGameByAvailability() {
+      const availability = document.getElementById("availabilitySelect").value;
+      if (availability === "") {
+        this.findGameByAvailabilityResult = [];
+      } else {
+        this.fetchFindGameByAvailability(availability === 'true');
+      }
+    },
+
+    fetchFindGameByGametype(gametype) {
+      axios
+          .get(`${this.api}/findGameByGametype/${gametype}`)
+          .then((res) => {
+            this.findGameByGametypeResult = res.data;
+            console.log("Games found:", res.data);
+          });
+    },
+
+    findGameByGametype() {
+      const gametype = document.getElementById("gametypeSelect").value;
+      if (gametype === "") {
+        this.findGameByGametypeResult = [];
+      } else {
+        this.fetchFindGameByGametype(gametype);
+      }
     },
 
     approveGame(id) {
@@ -174,16 +372,17 @@ export default {
           .catch(console.error)
     },
 
-    /*updateGame(id) {
+  /* updateGame(id) {
       axios
           .put(`${this.api}/updateGame/${id}`, this.updatedGame)
           .then((res) => {
             console.log('Game updated:', res.data);
             this.fetchGames();
-            this.newGame = {gamename: '', description: '', gametype: '', location: ''};
+            this.updatedGame = {gamename: '', description: '', gametype: '', location: ''};
           })
           .catch(console.error)
-    },*/
+    },
+    */
 
     deleteGameById(id) {
       axios
@@ -202,7 +401,3 @@ export default {
 };
 
 </script>
-
-<style>
-
-</style>
